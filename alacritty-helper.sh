d802c0cc873f3efbf11e910c7831c4e6993e5877
Conner Turner <cturner@zyme.xyz>
2022-03-25 00:51:41 +0000

    Starting alacritty helper script

---
+++ alacritty-helper.sh
@@ -0,0 +1,14 @@
+# Helper for updating, building etc Alacritty
+
+LATEST_RELEASE=0
+BUILD_DIRECTORY="/path/to/alacritty"
+get_latest_release() {
+	github_url="https://github.com/alacritty/alacritty/"
+	github_latest=$(basename $(curl -fs -o/dev/null -w %{redirect_url} $github_url"releases/latest"))
+	if [ $? -ne 0 ]; then
+		echo "Unable to get version, see output:"
+		echo $github_latest
+	else
+		LATEST_RELEASE=$github_latest
+	fi
+}
