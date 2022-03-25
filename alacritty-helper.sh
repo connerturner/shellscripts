996de9433e7cc42aef39f95beb313296c0690c0f
Conner Turner <cturner@zyme.xyz>
2022-03-25 02:28:46 +0000

    Build release with cargo

--- alacritty-helper.sh
+++ alacritty-helper.sh
@@ -1,7 +1,6 @@
 # Helper for updating, building etc Alacritty
-
 LATEST_RELEASE=0
-BUILD_DIRECTORY="/path/to/alacritty"
+BUILD_DIRECTORY="${ALACRITTY_DIRECTORY:~/alacritty}"
 get_latest_release() {
 	github_url="https://github.com/alacritty/alacritty/"
 	github_latest=$(basename $(curl -fs -o/dev/null -w %{redirect_url} $github_url"releases/latest"))
@@ -15,7 +14,7 @@ get_latest_release() {
 
 check_deps(){
 	# Ensure Rust is usable
-	type rustc >/dev/null 2>&1 || {
+	type rustc > /dev/null 2>&1 || {
 		echo >&2 "Could not find rustc try using rustup.rs"; 
 		exit 1
 	}
@@ -23,5 +22,17 @@ check_deps(){
 	deps=("cmake" "pkg-config" "libfreetype6-dev" "libfontconfig1-dev" "libxcb-xfixes0-dev" "libxkbcommon-dev" "python3")
 	dpkg -s "${deps[@]}" >/dev/null 2>&1 || {
 		echo "Deps not installed, need: ${deps[*]}"
+		exit 1
 	}
+}
+
+_build() {
+	[ -d $BUILD_DIRECTORY ] && cargo build --release
+}
+
+build() {
+	# Check for Dependencies first
+	check_deps
+	# Build release
+	_build || echo "Build failed. Aborting." && exit 1	
 }
